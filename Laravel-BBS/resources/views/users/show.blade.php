7085341dc455e70406765e007fbfc798480f9640
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2018-04-15 16:08:01 +0800

    User profile page

---
+++ Laravel-BBS/resources/views/users/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('title', $user->name . ' 的个人中心')
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="media">
+                    <div align="center">
+                        <img class="thumbnail img-responsive" src="https://lccdn.phphub.org/uploads/avatars/24325_1523760650.jpeg?imageView2/1/w/100/h/100" width="300px" height="300px">
+                    </div>
+                    <div class="media-body">
+                        <hr>
+                        <h4><strong>个人简介</strong></h4>
+                        <p>I don't Love u AnyMore ...</p>
+                        <hr>
+                        <h4><strong>注册于</strong></h4>
+                        <p>January 01 1901</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <span>
+                    <h1 class="panel-title pull-left" style="font-size:30px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+                </span>
+            </div>
+        </div>
+        <hr>
+
+        {{-- 用户发布的内容 --}}
+        <div class="panel panel-default">
+            <div class="panel-body">
+                暂无数据 ~_~
+            </div>
+        </div>
+
+    </div>
+</div>
+@stop

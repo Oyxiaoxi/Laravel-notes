3e81e43fced6c3d37e27f55360a3322275f71892
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2018-04-16 16:08:17 +0800

    User can create reply

--- Laravel-BBS/app/Observers/ReplyObserver.php
+++ Laravel-BBS/app/Observers/ReplyObserver.php
@@ -9,13 +9,15 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    // reply_count + 1
+    public function created(Reply $reply)
     {
-        //
+        $reply->topic->increment('reply_count', 1);
     }
 
-    public function updating(Reply $reply)
+    // clean()
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }

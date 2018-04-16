e7f5b398539867aa269271496ab096725c40bc22
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2018-04-16 13:17:49 +0800

    Prevent XSS injection

--- Laravel-BBS/app/Observers/TopicObserver.php
+++ Laravel-BBS/app/Observers/TopicObserver.php
@@ -21,6 +21,9 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        // 保存时利用 clean() 过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }

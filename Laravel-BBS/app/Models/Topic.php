d423fbde2bb257a63b41df160cb0321585a2ce8d
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2018-04-16 10:07:16 +0800

    Topic index page

--- Laravel-BBS/app/Models/Topic.php
+++ Laravel-BBS/app/Models/Topic.php
@@ -3,6 +3,20 @@
 namespace App\Models;
 
 class Topic extends Model
-{
+{   
+    /**
+     * 模型关联
+     * belongsTo()      
+    */
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
